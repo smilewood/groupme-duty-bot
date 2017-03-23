78c46afdacad897c49d5a199186486031fc77c03
CombustibleFox <tanner.rohnke@gmail.com>
2017-03-22 19:02:32 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,9 +13,9 @@ function respond() {
   //Arrays of duty partners
   var jan = ["no one", "no one", "no one", "no one", "no one", "no one", "no one", "Julia and Rachel T.", "Coby and Luke", "Emma and Tanner", "Braede and Gabby", "Frida and Matt", "Max and Kellie", "Austin and Ashton", "Luke and Ashton", "Max and Braede", "Luke and Frida", "Matt and Ashton", "Rachel T. and Frida", "Ashton and Luke", "Emma and Braede", "Emma and Braede", "Max and Luke", "Emma and Luke", "Matt and Coby", "Julia and Kellie", "Braded and Austin", "Ashton and Max", "Ashton and Max", "Max and Braede", "Tanner and Emma", "Coby and Max"]; 
   var feb = ["Rachel T. and Kellie", "Julia and Gabriella", "Luke and Austin", "Luke and Austin", "Frida and Max", "Tanner and Ashton", "Austin and Rachel M.", "Matt and Kellie", "Braede and Austin", "Rachel M. and Coby", "Rachel M. and Coby", "Rachel T. and Frida", "Tanner and Emma", "Ashton and Kellie", "Rachel T. and Gabriella", "Austin and Julia", "Matt and Frida", "Matt and Frida", "Coby and Max", "Tanner and Emma", "Rachel M. and julia", "Matt and Luke", "Coby and Frida", "Tanner and Kellie", "Tanner and Kellie", "Braede and Luke", "Luka and Kellie", "Austin and Gabriella", "", ""]; 
-  var mar = ["Matt and Rachel M.", "Austin and Emma", "Gabriella and Julia", "Gabriella and Julia", "Matt and Ashton", "Tanner and Emma", "Ashton and Gabriella", "Coby and Kellie", "Julia and Coby", "Luke and Frida", "Luke and Frida", "Some random people", "That one person", "?????????", "My favorite person whose name I forgot", "IDK who", "I can't always tell you who", "That one RA with the face and the one with the hair", "Max and Frida", "Tanner and Emma", "Rachel M. Gabriella", "Rachel T. and Julia", "Gabriella and Braede", "Coby and Ashton", "Coby and Ashton", "Max and Braede", "Julia and Kellie", "Rachel M. and Ashton", "Matt and Braede", "Austin and Emma", "Austin and Rachel T."]; 
-  var apr = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]; 
-  var may = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]; 
+  var mar = ["Matt and Rachel M.", "Austin and Emma", "Gabriella and Julia", "Gabriella and Julia", "Matt and Ashton", "Tanner and Emma", "Ashton and Gabriella", "Coby and Kellie", "Julia and Coby", "Luke and Frida", "Luke and Frida", "Some random people", "That one person", "?????????", "My favorite person whose name I forgot", "IDK who", "I can't always tell you who", "That one RA with the face and the one with the hair", "Max and Frida", "Tanner and Emma", "Rachel M. Gabriella", "Gabriella and Julia", "Rachel M. and Braede", "Coby and Luke", "Coby and Luke", "Max and Braede", "Julia and Kellie", "Rachel M. and Ashton", "Matt and Braede", "Austin and Emma", "Austin and Rachel T."]; 
+  var apr = ["Austin and Telljohn", "Max and Kellie", "Tanner and Luke", "Austin and Telljohn", "McCormick and Kellie", "Julia and Gabby", "McCormick and Braede", "McCormick and Braede", "Telljohn and Frida", "Tanner and Max", "Coby and Gabby", "Julia and McCormick", "Frida and Braede", "Emma and Max", "Emma and Max", "Julia and Matt", "Tanner and Kellie", "Matt and Gabby", "Coby and McCormick", "Braede and Austin", "Matt and Telljohn", "Matt and Telljohn", "Luke and Emma", "Tanner and Ashton", "Telljohn and Gabby", "Frida and Matt", "Coby and Austin", "Kellie and Julia", "Kellie and Julia", "Telljohn and Frida",]; 
+  var may = ["Ashton and Emma", "Telljohn and McCormick", "Emma and Gabby", "Coby and McCormick", "Tanner and Gabriella", "Tanner and Gabriella", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]; 
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -44,6 +44,12 @@ function respond() {
     else if (month == 2) {
       people = mar[day-1];
     }
+    else if (month == 3) {
+      people = apr[day-1];
+    }
+    else if (month == 4) {
+      people = may[day-1];
+    }
     postMessage("Today " + people + " are on duty");
     this.res.end();
   } 
@@ -61,6 +67,12 @@ function respond() {
     }
     else if (month == 2) {
       people = mar[day];
+    }
+     else if (month == 3) {
+      people = apr[day];
+    }
+    else if (month == 4) {
+      people = may[day];
     }
     
     postMessage("Tomorrow " + people + " are on duty");
@@ -83,7 +95,6 @@ function respond() {
   }  
   else if(request.text && botRegexBirb.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/CcQTXgp.jpg");
     postMessage("http://imgur.com/dwRfrQm");
     this.res.end();
   } 
