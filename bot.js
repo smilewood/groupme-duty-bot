5266e5cc9b49faf131b9a2f64a9665c5ce7ecdae
ewanlp <ewanlp@gmail.com>
2017-08-23 11:23:06 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,7 +225,7 @@ function respond() {
   } 
   else if(request.text && botRegexUp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("New in this update:\nAdded functions:\n\n/birdsupport\n/v\n");
+    postMessage("New in this update:\nAdded functions:\n\n/birdsupport\n/v\n/thisweek\n");
     this.res.end();
   } 
   else if(request.text && botRegexSor.test(request.text)) {
