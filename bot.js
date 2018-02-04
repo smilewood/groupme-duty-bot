babccd32799ae4a5e2055d9a419991f3eba31d64
ewanlp <ewanlp@gmail.com>
2018-02-04 16:40:32 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ function respond() {
 
   
   var mon = [["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], 
+  ["", "", "", "", "McKenzie and Mike", "Christian and Emma", "Ally and Austin", "Taylor and Cecilia", "Paige and Mike", "Luke and Mike", "Rachel and Ally", "Cecilia and ", "Ally and Ashton", "Jordan and Paige", "David and ", "David and ", "Luke and Mike", "Paige and Makenzie", "Emma and Rachel", "Ally and Luke", "David and Ashton", "Emma and Ally", "Mike and Ally", "Rachel and ", "Emma and Rachel", "David and Ashton", "Christian and Ally", "", "", "", "", ""], 
   ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], 
   ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], 
   ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]];
